ad62cfd6da193152045d9e8067b8fe1b0058db26
Michael Theis (msg) <michael.theis@msg.group>
2022-04-06 12:17:08 +0200

    added new stuff to s3 bucket to be compliant with common security requirements
    - s3 bucket is now encrypted using AES256 encryption
    - all public access to s3 bucket is blocked
    - encrypted access to s3 bucket is enforced now (no HTTP connections!)
    - versioning is enabled on s3 bucket now

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# iac-tf-aws-backend-module: Terraform module to set up a Terraform backend based on S3 and DynamoDB
+# iac-tf-aws-backend-module 
+
+Terraform module to set up a Terraform remote backend based on S3 and DynamoDB.
 
 Creates a S3 bucket for state storage and a DynamoDB table for state locking.
+
+> This module tries to be as compliant as possible with CSI benchmarks and AWS best practices.
  
 ## Input Variables
 
--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,15 +1,58 @@
 resource aws_s3_bucket backend {
   bucket = local.s3_bucket_name
-  versioning {
-    enabled = true
-  }
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
   tags = merge({Name = local.s3_bucket_name}, local.module_common_tags)
 }
 
+// enable versioning on S3 bucket to be able to recover from unintended state changes
+resource aws_s3_bucket_versioning backend {
+  bucket = aws_s3_bucket.backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+// enable default encryption for data-at-rest with SSE-S3 encryption
+resource aws_s3_bucket_server_side_encryption_configuration backend {
+  bucket = aws_s3_bucket.backend.id
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+// block public access to bucket
+resource aws_s3_bucket_public_access_block block_public_access {
+  bucket = aws_s3_bucket.backend.id
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+// add a bucket policy which denies HTTP access
+resource aws_s3_bucket_policy deny_http_access {
+  bucket = aws_s3_bucket.backend.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "BUCKET-POLICY"
+    Statement = [
+      {
+        Sid       = "EnforceTls"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          "${aws_s3_bucket.backend.arn}/*",
+          "${aws_s3_bucket.backend.arn}",
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+          NumericLessThan = {
+            "s3:TlsVersion": 1.2
+          }
+        }
+      },
+    ]
+  })
+}
